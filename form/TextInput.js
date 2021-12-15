0cee626ae8b618e6353c301d0519826e4ea1ec06
kennck <kennettecanales@gmail.com>
2021-12-15 16:51:53 +0800

    update

--- form/TextInput.js
+++ form/TextInput.js
@@ -5,7 +5,21 @@ export default class TextInput extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      error: 'This is a test'
     };
+  }
+
+  validation = () => {
+    const { validation, value } = this.props;
+    if(validation && value){
+      if(validation.type == 'text'){
+        if(validation.size && value.length < validation.size){
+          this.setState({
+            error: 'Minimum required is ' + validation.size
+          })
+        }
+      }
+    }
   }
 
   render() {
@@ -24,22 +38,23 @@ export default class TextInput extends React.Component {
             )
           }
 
-          {
-            this.props.error && (
-              <label style={{
-                fontWeight: 'bold',
-                color: Colors.danger
-              }}>{this.props.error}</label>
-            )
-          }
-
           <input
             type={this.props.type}
             placeholder={this.props.placeholder}
             style={{...BasicStyles.formControl, ...this.props.style}}
             value={this.props.value}
-            onChange={(e) => this.props.onChange(e.value)}
+            onChange={(e) => {
+              this.props.onChange(e.value)
+              this.validation()
+            }}
             ></input>
+          {
+            this.state.error && (
+              <label style={{
+                color: Colors.danger
+              }}><b>Oops!</b> {this.state.error}</label>
+            )
+          }
         </div>
     )
   }
