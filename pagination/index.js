ceb43117f418010fc8a9ff74c9864107390ed49f
kennck <kennettecanales@gmail.com>
2022-01-19 17:28:39 +0800

    update

--- pagination/index.js
+++ pagination/index.js
@@ -7,29 +7,44 @@ export default class TextInput extends React.Component {
     super(props);
   }
 
+  onPrev(){
+    const { selected } = this.props;
+    if(selected > 0){
+      this.props.onRetrieve(selected - 1)
+    }
+  }
+
+  onNext(){
+    const { selected, last } = this.props;
+    if(selected < last){
+      this.props.onRetrieve(selected + 1)
+    }
+  }
+
 
   render() {
+    const { pages, selected } = this.props;
     return (
         <div style={{
           display: 'flex',
           justifyContent: 'right'
         }}>
         <Pagination size="lg">
-          <Pagination.First />
-          <Pagination.Prev />
-          <Pagination.Item>{1}</Pagination.Item>
-          <Pagination.Ellipsis />
-
-          <Pagination.Item>{10}</Pagination.Item>
-          <Pagination.Item>{11}</Pagination.Item>
-          <Pagination.Item active>{12}</Pagination.Item>
-          <Pagination.Item>{13}</Pagination.Item>
-          <Pagination.Item disabled>{14}</Pagination.Item>
-
-          <Pagination.Ellipsis />
-          <Pagination.Item>{20}</Pagination.Item>
-          <Pagination.Next />
-          <Pagination.Last />
+          <Pagination.First 
+            onClick={() => this.props.onRetrieve(0)}/>
+          <Pagination.Prev 
+            onClick={() => this.onPrev()} />
+          {
+            pages && pages.map((item) => (
+              <Pagination.Item
+                onClick={() => this.props.onRetrieve(item)}
+                >{item}</Pagination.Item>
+            ))
+          }
+          <Pagination.Next 
+            onClick={() => this.onNext()}/>
+          <Pagination.Last
+            onClick={() => this.props.onRetrieve(this.props.last)}/>
         </Pagination>
       </div>
     )
