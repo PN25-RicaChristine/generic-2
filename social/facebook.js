d510d7ac7903c3cc537b24542bd7fb722c7c3e72
kennck <kennettecanales@gmail.com>
2022-03-04 11:05:05 +0800

    update

--- social/facebook.js
+++ social/facebook.js
@@ -8,14 +8,44 @@ export default class Stack extends React.Component {
   constructor(props) {
     super(props);
   }
+
+  init = () => {
+    console.log('hello')
+    let FB = window.FB
+    FB.init({
+      appId            : config.facebook.id,
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v13.0'
+    });
+
+    FB.login(function(response) {
+      if (response.authResponse) {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me', function(response) {
+          console.log('Good to see you, ' + response.name + '.');
+        });
+      } else {
+        console.log('User cancelled login or did not fully authorize.');
+      }
+    });
+  }
   
   render() {
     return (
-        <div>
-            <FontAwesomeIcon
-                icon={faFacebookF}
-                size="1x"
-                />
+        <div
+          onClick={() => {
+            this.init()
+          }}
+        >
+          <FontAwesomeIcon
+              icon={faFacebookF}
+              size="1x"
+              />
+
+          <Helmet>
+            <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"></script>
+          </Helmet>
         </div>
     )
   }
