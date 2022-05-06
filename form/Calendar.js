a6d4728ae698983fa0db76c19fbb625ab371aa09
chloe868 <herreracherrymae@gmail.com>
2022-05-06 10:51:56 +0800

    Updates

--- form/Calendar.js
+++ form/Calendar.js
@@ -6,77 +6,95 @@ import 'react-calendar/dist/Calendar.css';
 import Calendar from 'react-calendar';
 
 export default class Stack extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            show: false
-        };
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			show: false
+		};
+	}
 
-    getMonthEquivalent(month){
-        switch(month){
-            case 0: return 'January'
-            case 1: return 'Febuary'
-            case 2: return 'March'
-            case 3: return 'April'
-            case 4: return 'May'
-            case 5: return 'June'
-            case 6: return 'July'
-            case 7: return 'August'
-            case 8: return 'September'
-            case 9: return 'October'
-            case 10: return 'November'
-            case 11: return 'December'
-        }
-    }
-    onChange = (e) => {
-        const year = e.getFullYear()
-        const date = e.getDate()
-        const month = this.getMonthEquivalent(e.getMonth())
-        const valueLabel = month + ' ' + date + ', ' + year
-        this.props.onChange(e, valueLabel)
-    }
+	getMonthEquivalent(month) {
+		switch (month) {
+			case 0: return 'January'
+			case 1: return 'Febuary'
+			case 2: return 'March'
+			case 3: return 'April'
+			case 4: return 'May'
+			case 5: return 'June'
+			case 6: return 'July'
+			case 7: return 'August'
+			case 8: return 'September'
+			case 9: return 'October'
+			case 10: return 'November'
+			case 11: return 'December'
+		}
+	}
+	onChange = (e) => {
+		const year = e.getFullYear()
+		const date = e.getDate()
+		const month = this.getMonthEquivalent(e.getMonth())
+		const valueLabel = month + ' ' + date + ', ' + year
+		this.props.onChange(e, valueLabel)
+	}
 
-    render() {
-        const { show, value, valueLabel } = this.state;
-        return (
-            <div style={{
-                width: '100%',
-                float: 'left',
-                ...BasicStyles.formControlContainer
-            }}>
-                <div style={{
-                    ...BasicStyles.formControl,
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'space-between'
-                }}
-                    className="cursor-hover"
-                    onClick={() => {
-                        this.setState({
-                            show: !show
-                        })
-                    }}
-                >
-                    <span>{this.props.valueLabel ? this.props.valueLabel : this.props.placeholder}</span>
-                    <span style={{
-                        paddingLeft: 20
-                    }}>
-                        <FontAwesomeIcon icon={faCalendar} size="lg" />
-                    </span>
+	render() {
+		const { show, value, valueLabel } = this.state;
+		return (
+			<div style={{
+				width: '100%',
+				float: 'left',
+				...BasicStyles.formControlContainer
+			}}>
+				<div style={{
+					...BasicStyles.formControl,
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'space-between'
+				}}
+					className="cursor-hover"
+					onClick={() => {
+						this.setState({
+							show: !show
+						})
+					}}
+				>
+					<span>{this.props.valueLabel ? this.props.valueLabel : this.props.placeholder}</span>
+					<span style={{
+						paddingLeft: 20
+					}}>
+						<FontAwesomeIcon icon={faCalendar} size="lg" />
+					</span>
+					{
+						this.props.iconRight && (
+							<span style={{
+								width: '15%',
+								float: 'left',
+								display: 'flex',
+								justifyContent: 'right',
+								alignItems: 'center',
+								height: 50
+							}}
+								className="href-link"
+								onClick={() => {
+									this.props.onClickRightIcon()
+								}}
+							>
+								<FontAwesomeIcon icon={this.props.iconRight} color={this.props.iconStyle != null ? this.props.iconStyle : 'black'} size="lg" />
+							</span>
+						)
+					}
 
-                </div>
-                {
-                    show && (
-                        <div style={{
-                            position: 'absolute'
-                        }}>
-                            <Calendar onChange={this.onChange} value={this.state.value} />
-                        </div>
-                    )
-                }
-
-            </div>
-        )
-    }
+				</div>
+				{
+					show && (
+						<div style={{
+							position: 'absolute'
+						}}>
+							<Calendar onChange={this.onChange} value={this.state.value} minDate={new Date()} />
+						</div>
+					)
+				}
+			</div>
+		)
+	}
 }
