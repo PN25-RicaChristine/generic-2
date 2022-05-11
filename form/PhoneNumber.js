530027d7a52d0b4892c5058a03c86990eaf86c14
kennck <kennettecanales@gmail.com>
2022-05-11 10:30:28 +0800

    update

--- form/PhoneNumber.js
+++ form/PhoneNumber.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import 'react-phone-number-input/style.css'
+import './PhoneNumber.css'
 import PhoneInput from 'react-phone-number-input'
+import { BasicStyles } from 'common';
 export default class TextInput extends React.Component {
     constructor(props) {
         super(props);
@@ -16,10 +18,28 @@ export default class TextInput extends React.Component {
                 width: '100%',
                 float: 'left'
             }}>
-                <PhoneInput
-                    country="US"
-                    value={value}
-                    onChange={setValue} />
+                {
+                    this.props.label && (
+                        <label style={{
+                            paddingTop: 10,
+                            paddingBottom: 10,
+                            fontWeight: 'bold'
+                        }}>{this.props.label}</label>
+                    )
+                }
+                <div style={{
+                    width: '100%',
+                    float: 'left'
+                }}>
+                    <PhoneInput
+                        country={this.props.defaultCountry}
+                        defaultCountry={this.props.defaultCountry}
+                        value={this.props.value}
+                        placeholder={this.props.placeholder}
+                        style={BasicStyles.formControl}
+                        onChange={this.onChange} />
+                </div>
+
             </div>
         )
     }
