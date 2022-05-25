f10108aacb04ee2cbe74d9c2ad19b3394c8460e5
kennck <kennettecanales@gmail.com>
2022-05-25 11:02:47 +0800

    update

--- form/AutoComplete.js
+++ form/AutoComplete.js
@@ -1,16 +1,39 @@
 import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
-
+import Colors from 'common/Colors'
 export default function Stack(props) {
-  return (
-    <Autocomplete
-      disablePortal
-      options={props.data}
-      sx={{ width: 300 }}
-      renderInput={(params) => <TextField {...params} label={props.label} />}
-    />
-  );
+    return (
+        <Autocomplete
+            disablePortal
+            options={props.data}
+            sx={{
+                width: '100%',
+                float: 'left',
+                border: 'none',
+                borderBottom: 'solid 2px ' + Colors.gray
+            }}
+            renderInput={(params) =>
+                <TextField
+                    {...params}
+                    disableUnderline={true}
+                    label={props.label}
+                    variant="standard"
+                    inputProps={{
+                        underline: {
+                            "&&&:before": {
+                                borderBottom: "none"
+                            },
+                            "&&:after": {
+                                borderBottom: "none"
+                            }
+                        }
+                    }}
+
+                />
+            }
+        />
+    );
 }
 
 
