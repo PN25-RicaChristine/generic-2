62a54e60364e322e046d6b42b30b6d97d2940a60
kennck <kennettecanales@gmail.com>
2022-06-10 16:03:22 +0800

    udpate

--- form/AutoComplete.js
+++ form/AutoComplete.js
@@ -3,22 +3,30 @@ import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import Colors from 'common/Colors'
 export default function Stack(props) {
+
+    console.log({
+        data: props.data
+    })
     return (
         <Autocomplete
-            disablePortal
+            multiple
             options={props.data}
+            id="tags-standard"
             sx={{
                 width: '100%',
                 float: 'left',
                 border: 'none',
                 borderBottom: 'solid 2px ' + Colors.gray
             }}
+            defaultValue={[props.data[0]]}
+            isOptionEqualToValue={(option, value) => option.title}
+            getOptionLabel={(option) => option.title}
             renderInput={(params) =>
                 <TextField
                     {...params}
                     label={props.label}
                     variant="standard"
-
+                    placeholder={props.label}
                 />
             }
         />
